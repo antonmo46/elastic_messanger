4005dfe5fcba93dfffba295e2d7f0ba6c24c5da7
Anton <abardakhan@gmail.com>
2015-05-07 14:11:23 -0700

    aws, login passport

--- moscow/config/env/development.js
+++ moscow/config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
-	db: 'mongodb://localhost/mean-book',
-	sessionSecret: 'developmentSessionSecret'
+	db : 'mongodb://localhost/mean-book',
+	sessionSecret : 'developmentSessionSecret',
+	facebook : {
+		clientID : '1436510586665106',
+		clientSecret : '6157bf791104defdfd15778d95b98c10',
+		callbackURL : 'http://localhost:3000/oauth/facebook/callback'
+	}
 };
--- moscow/config/express.js
+++ moscow/config/express.js
@@ -1,10 +1,13 @@
 var config = require('./config'),
-express = require('express'),
-morgan = require('morgan'),
-compress = require('compression'),
-bodyParser = require('body-parser'),
-methodOverride = require('method-override'),
-session = require('express-session');
+	express = require('express'),
+	morgan = require('morgan'),
+	compress = require('compression'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	passport = require('passport');
+
 module.exports = function () {
 	var app = express();
 	if (process.env.NODE_ENV === 'development') {
@@ -24,6 +27,11 @@ module.exports = function () {
 		}));
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
+	
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+
 	require('../app/routes/index.server.routes.js')(app);
 	require('../app/routes/users.server.routes.js')(app);
 	app.use(express.static('./public'));
